e6a9b49778a8a010c90bdc39e84981dbb94a0da2
Ee Lou <eelou.sai@gmail.com>
2022-12-20 01:15:59 +0000

    Before the start of mortgage program.

--- lesson2/lesson_exercises/walk_through_refactoring_calculator/refactoring_calculator.js
+++ lesson2/lesson_exercises/walk_through_refactoring_calculator/refactoring_calculator.js
@@ -4,58 +4,74 @@
 // Peform the operation on the two numbers.
 // Print the result to the terminal.
 
+const MESSAGES = require('./calculator_messages.json');
 const readline = require('readline-sync');
+const LANGUAGE = 'en';
 
-function prompt (message) {
+function prompt (key) {
+  let message = messages(key, LANGUAGE);
   console.log(`=> ${message}`);
+}
+
+function messages(message, lang = "en") {
+  return MESSAGES[lang][message];
 }
 
 function invalidNumber (num) {
   return num.trimStart() === '' || Number.isNaN(Number(num));
 }
 
-prompt('Welcome to Calculator!');
+prompt('welcome');
 
-prompt('What\'s the first number?');
-let num1 = readline.question();
-
-while (invalidNumber(num1)) {
-  prompt(`Hm... that doesn't look like a valid number.`);
-  num1 = readline.question();
-}
-
-prompt('What\'s the second number?');
-let num2 = readline.question();
-
-while (invalidNumber(num1)) {
-  prompt(`Hm... that doesn't look like a valid number.`);
-  num2 = readline.question();
-}
-
-prompt('What operation would you like to perform?\n=> 1) Add\n=> 2) Subtract\n=> 3) Multiply\n=> 4) Divide');
-let operation = readline.question();
-
-//while (!['1', '2', '3', '4'].includes(operation))
-while (operation !== '1' && operation !== '2' && operation !== '3' && operation !== '4') {
-  prompt(`Must choose 1, 2, 3, or 4`);
-  operation = readline.question();
-}
-
-let result;
-
-switch (operation) {
-  case '1':
-    result = Number(num1) + Number(num2);
+while (true) {
+  prompt('firstNumber');
+  let num1 = readline.question();
+  
+  while (invalidNumber(num1)) {
+    prompt(`errorNumber`);
+    num1 = readline.question();
+  }
+  
+  prompt('secondNumber');
+  let num2 = readline.question();
+  
+  while (invalidNumber(num2)) {
+    prompt(`errorNumber`);
+    num2 = readline.question();
+  }
+  
+  prompt('performOperation');
+  let operation = readline.question();
+  
+  //while (!['1', '2', '3', '4'].includes(operation))
+  while (operation !== '1' && operation !== '2' && operation !== '3' && operation !== '4') {
+    prompt(`wrongChoice`);
+    operation = readline.question();
+  }
+  
+  let result;
+  
+  switch (operation) {
+    case '1':
+      result = Number(num1) + Number(num2);
+      break;
+    case '2':
+      result = Number(num1) - Number(num2);
+      break;
+    case '3':
+      result = Number(num1) * Number(num2);
+      break;
+    case '4':
+      result = Number(num1) / Number(num2);
+      break;
+  }
+  
+  console.log(`The result is: ${result}`);
+  
+  prompt(`onemore`);
+  let answer = readline.question();
+  
+  if (answer[0].toLowerCase() !== 'y') {
     break;
-  case '2':
-    result = Number(num1) - Number(num2);
-    break;
-  case '3':
-    result = Number(num1) * Number(num2);
-    break;
-  case '4':
-    result = Number(num1) / Number(num2);
-    break;
+  }
 }
-
-prompt(`The result is: ${result}`);
