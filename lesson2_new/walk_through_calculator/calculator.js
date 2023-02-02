18888d941e9dee5357205486ec0b1ceb0fcc0f24
Ee Lou <eelou.sai@gmail.com>
2023-02-02 06:49:19 +0000

    adding while loop for continuous program

--- lesson2_new/walk_through_calculator/calculator.js
+++ lesson2_new/walk_through_calculator/calculator.js
@@ -15,44 +15,57 @@ function invalidNumber(number) {
 
 prompt("Welcome to Calculator!");
 
-prompt("What's the first number?");
-let number1 = readline.question();
+while (true) {
+  prompt("What's the first number?");
+  let number1 = readline.question();
 
-while (invalidNumber(number1)) {
+  while (invalidNumber(number1)) {
   prompt("Hmm... that doesn't look like a valid number.");
   number1 = readline.question();
-}
+  }
 
-prompt("What's the second number?");
-let number2 = readline.question();
+  prompt("What's the second number?");
+  let number2 = readline.question();
 
-while (invalidNumber(number2)) {
-  prompt("Hmm... that doesn't look like a valid number.");
-  number2 = readline.question();
-}
-
-prompt("What operation would you like to perform?\n1) Add\n2) Subtract\n3) Multiply\n4) Divide");
-let operation = readline.question();
-
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt("Must choose 1, 2, 3, or 4");
-  operation = readline.question();
-}
-
-let output;
-switch (operation) {
-  case '1':
-    output = Number(number1) + Number(number2);
+  while (invalidNumber(number2)) {
+    prompt("Hmm... that doesn't look like a valid number.");
+    number2 = readline.question();
+  }
+  
+  prompt("What operation would you like to perform?\n=> 1) Add\n=> 2) Subtract\n=> 3) Multiply\n=> 4) Divide");
+  let operation = readline.question();
+  
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt("Must choose 1, 2, 3, or 4");
+    operation = readline.question();
+  }
+  
+  let output;
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+  }
+  
+  prompt(`The result is: ${output}.`);
+  
+  prompt(`Do you want to do more calculation?`);
+  let answer = readline.question();
+  
+  if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
     break;
-  case '2':
-    output = Number(number1) - Number(number2);
-    break;
-  case '3':
-    output = Number(number1) * Number(number2);
-    break;
-  case '4':
-    output = Number(number1) / Number(number2);
-    break;
+  }else if (answer.toLowerCase() !== 'yes' || answer.toLowerCase() !== 'y'
+            || answer.toLowerCase() !== 'no' || answer.toLowerCase() !== 'n') {
+    prompt(`Please enter 'yes' or 'no'.`);
+    answer = readline.question();
+  }
 }
-
-prompt(`The result is: ${output}.`);
